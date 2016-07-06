4485afc7713db8df31342799aea0045e28fc6e9e
artem-dudarev <ad@virtoway.com>
2016-07-06 15:46:41 +0200

    Added theme

---
+++ theme/assets/scripts/angular/common.js
@@ -0,0 +1,31 @@
+﻿var storefrontApp = angular.module('storefrontApp');
+
+storefrontApp.controller('mainController', ['$scope', '$window', '$location', '$anchorScroll', 'feedbackService', function ($scope, $window, $location, $anchorScroll, feedbackService) {
+    $scope.formModel = {};
+    $scope.$location = $location;
+
+    $scope.scrollTo = function (id) {
+        var old = $location.hash();
+        $location.hash(id);
+        $anchorScroll();
+        $location.hash(old);
+    }
+
+    $scope.setForm = function (form) {
+        $scope[form.$name] = form;
+        $scope.formModel = {};
+    }
+
+    $scope.submit = function (form) {
+        form.$setSubmitted();
+        if (form.$invalid) {
+            return;
+        }
+        feedbackService.postFeedback($scope.formModel).then(function (response) {
+            if ($scope.formModel.redirectUrl) {
+                $window.location.href = $scope.formModel.redirectUrl;
+            }
+        }, function (response) {
+        });
+    }
+}]);
---
+++ theme/assets/scripts/hider.js
@@ -0,0 +1,28 @@
+﻿$(function () {
+	var seoContent = {
+		"1": "VG8gbGVhcm4gbW9yZSBhYm91dCBFbnRlcnByaXNlIGVkaXRpb24gcHJpY2luZywgcGxlYXNlIGNvbXBsZXRlIHRoZSBicmllZiBmb3JtIGFuZCB3ZSB3aWxsIGJlIGluIGNvbnRhY3Qgd2l0aCB5b3Ugc2hvcnRseQ==",
+		"2": "UHJvZHVjdGlvbiBMaWNlbnNlIENvbW11bml0eSBFZGl0aW9uIG9mIFZpcnRvIENvbW1lcmNlIGlzIG9mZmVyZWQgZnJlZSBvZiBjaGFyZ2UNCmlmIHlvdSBtZWV0IHRoZSByZXF1aXJlbWVudHMgb2YgdGhlIENvbW11bml0eSBFZGl0aW9uIExpY2Vuc2UgKG1haW5seSByZXZlbnVlIHJlc3RyaWN0aW9uKQ=="
+	};
+	var seoHrefs = {
+		"1": "L3RyeS1ub3c=",
+		"2": "L3RyeS1ub3c=",
+		"3": "L3RyeS1ub3c=",
+		"4": "L3RyeS1ub3c=",
+	};
+	var $elements = $("[id*='href'],[id*='cont']");
+	for (var i = 0; i < $elements.length; i++) {
+		var $element = $elements.eq(i);
+		var id = $element.attr("id");
+		var type = id.substring(0, 4);
+		var key = id.substring(5, id.length);
+		switch (type) {
+			case "href":
+				$element.attr("href", Base64.decode(seoHrefs[key]));
+				break;
+			case "cont":
+				$element.html(Base64.decode(seoContent[key]));
+				break;
+		}
+	}
+	$(document).trigger("renderpage.finish");
+});
