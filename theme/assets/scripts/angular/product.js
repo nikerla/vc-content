c53c0468eb92672fa2347bbf1f6dc2e58edd72c2
artem-dudarev <ad@virtoway.com>
2016-07-15 18:22:19 +0200

    Added styles for tabs. Added 'become a developer' form.

--- theme/assets/scripts/angular/product.js
+++ theme/assets/scripts/angular/product.js
@@ -24,7 +24,7 @@ storefrontApp.controller('productController', ['$scope', '$window', '$sce', 'cat
             var product = response.data[0];
             $scope.product = product;
             selectVariation(product.variations[0].id);
-            var fullDescription = _.find(product.descriptions, function (d) { return d.reviewType === 'FullReview' });
+            var fullDescription = _.find(product.properties, function (p) { return p.name === 'Overview' });
             if (fullDescription) {
                 $scope.fullDescription = $sce.trustAsHtml(fullDescription.value);
             }
@@ -34,11 +34,12 @@ storefrontApp.controller('productController', ['$scope', '$window', '$sce', 'cat
     function selectVariation(variationId) {
         var variation = _.find($scope.product.variations, function (v) { return v.id === variationId });
         if (variation) {
+            variation.locale = _.find(variation.properties, function (p) { return p.name === 'Locale' });
             variation.versionNumber = _.find(variation.variationProperties, function (p) { return p.name === 'ReleaseVersion' });
             variation.releaseDate = _.find(variation.variationProperties, function (p) { return p.name === 'ReleaseDate' });
             variation.compatibility = _.find(variation.variationProperties, function (p) { return p.name === 'Compatibility' });
-            variation.locale = _.find(variation.properties, function (p) { return p.name === 'Locale' });
-            variation.releaseNotes = _.find(variation.variationProperties, function (p) { return p.name === 'ReleaseNotes' });
+            variation.releaseNotes = _.find(variation.variationProperties, function (p) { return p.name === 'ReleaseNote' });
+            variation.downloadLink = _.find(variation.variationProperties, function (p) { return p.name === 'DowloadLink' });
             $scope.selectedVariation = angular.copy(variation);
         }
     }
