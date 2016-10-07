5c63c329b7ce573a768676a08f01b28f36c81ecc
Andrew Orlov <ao@virtoway.com>
2016-10-07 10:46:38 +0200

    VC docs menu restructure (in progress)

--- theme/assets/scripts/angular/docs.js
+++ theme/assets/scripts/angular/docs.js
@@ -1,11 +1,26 @@
-var storefrontApp = angular.module('storefrontApp', []);
+var storefrontApp = angular.module('storefrontApp', ['pascalprecht.translate']);
+
+storefrontApp.config(['$translateProvider', function ($translateProvider) {
+    $translateProvider.useSanitizeValueStrategy('sanitizeParameters');
+    $translateProvider.useUrlLoader(BASE_URL + 'themes/localization.json');
+    $translateProvider.preferredLanguage('en');
+}]);
+
+storefrontApp.controller('docsController', ['$scope', '$window', '$http', function ($scope, $window, $http) {
+    $scope.getMenuItems = function () {
+        $http.get(BASE_URL + 'themes/assets/docs-menu.json').then(function (response) {
+            var docTypeMenuItem = _.find(response.data, function (rootMenuItem) { return $window.pageUrl.indexOf(rootMenuItem.url) >= 0; });
+            $scope.menuItems = docTypeMenuItem ? docTypeMenuItem.submenu : response.data;
+        });
+    }
+}]);
 
 storefrontApp.component('vcDocsMenu', {
     templateUrl: 'themes/assets/docs-menu.tpl.html',
     bindings: {
-
-    },
-    controller: ['$scope', function ($scope) {
-        var ctrl = this;
-    }]
+        items: '=',
+        url: '=',
+        parent: '=',
+        isSubmenu: '='
+    }
 });
