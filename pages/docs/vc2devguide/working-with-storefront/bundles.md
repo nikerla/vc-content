5ebd4fce2a7cce908592ec6885f8e30ded64c028
Andrew Orlov <ao@virtoway.com>
2016-10-10 16:26:53 +0200

    Changes to VC docs menu, added "Sorting" field, removed permalinks, getting pages URLs from file system

---
+++ pages/docs/vc2devguide/working-with-storefront/bundles.md
@@ -0,0 +1,17 @@
+---
+title: Bundles
+layout: docs
+date: 2016-05-16T10:49:50.627Z
+sorting: 7
+---
+# Bundles
+
+Open the App_Start\BundleConfig.cs file and examine the RegisterBundles method which is used to create, register and configure bundles. The following code shows a portion of the RegisterBundles method.
+
+![](assets/images/docs/budnles1.png)
+
+This code creates new JavaScript and CSS bundles for Storefront and Checkout layouts.
+
+After you have registered your bundles you can easily add it to your theme layout using **script_bundle_tag** liquid tag.
+
+![](assets/images/docs/bundles2.png)
--- theme/assets/scripts/angular/docs.js
+++ theme/assets/scripts/angular/docs.js
@@ -1,26 +1,75 @@
-var storefrontApp = angular.module('storefrontApp', ['pascalprecht.translate']);
+var storefrontApp = angular.module('storefrontApp', []);
 
-storefrontApp.config(['$translateProvider', function ($translateProvider) {
-    $translateProvider.useSanitizeValueStrategy('sanitizeParameters');
-    $translateProvider.useUrlLoader(BASE_URL + 'themes/localization.json');
-    $translateProvider.preferredLanguage('en');
-}]);
+storefrontApp.controller('docsController', ['$scope', '$window', function ($scope, $window) {
+    $scope.menuItems = buildMenu($window.menuItems, $window.pageUrl);
 
-storefrontApp.controller('docsController', ['$scope', '$window', '$http', function ($scope, $window, $http) {
-    $scope.getMenuItems = function () {
-        $http.get(BASE_URL + 'themes/assets/docs-menu.json').then(function (response) {
-            var docTypeMenuItem = _.find(response.data, function (rootMenuItem) { return $window.pageUrl.indexOf(rootMenuItem.url) >= 0; });
-            $scope.menuItems = docTypeMenuItem ? docTypeMenuItem.submenu : response.data;
+    function buildMenu(menuItems, pageUrl) {
+        _.each(menuItems, function (menuItem) {
+            menuItem.parents = getMenuItemParents(menuItem, menuItems);
         });
+        _.each(menuItems, function (menuItem) {
+            var submenu = getMenuItemSubmenu(menuItem, menuItems);
+            var hasActiveSubmenuItem = _.some(submenu, function (item) { return item.url === pageUrl });
+            submenu.expanded = (menuItem.url == pageUrl) || hasActiveSubmenuItem;
+            menuItem.submenu = submenu;
+        });
+        var pageUrlParts = pageUrl.split('/');
+        menuItems = _.find(menuItems, function (menuItem) { return menuItem.url === pageUrlParts[0] }).submenu;
+        if (pageUrlParts.length >= 2) {
+            var docTypeUrl = pageUrlParts[0] + '/' + pageUrlParts[1];
+            menuItems = _.find(menuItems, function (menuItem) { return menuItem.url === docTypeUrl }).submenu;
+        }
+        return _.sortBy(menuItems, function (menuItem) { return menuItem.sorting });
+    }
+
+    function getMenuItemSubmenu(menuItem, menuItems) {
+        var submenu = _.filter(menuItems, function (item) {
+            var lastParent = item.parents[item.parents.length - 1];
+            return lastParent && (menuItem.url === lastParent.url);
+        });
+        return _.sortBy(submenu, function (item) { return item.sorting });
+    }
+
+    function getMenuItemParents(menuItem, allMenuItems) {
+        var parents = [];
+        var parentUrlParts = getParentUrlParts(menuItem);
+        var parentUrls = getParentUrls(parentUrlParts);
+        _.each(parentUrls, function (parentUrl) {
+            var parent = _.find(allMenuItems, function (item) { return item.url === parentUrl });
+            if (parent) {
+                parents.push(parent);
+            }
+        });
+        return parents;
+    }
+
+    function getParentUrls(urlParts) {
+        var urls = [];
+        for (var i = 0; i < urlParts.length; i++) {
+            var subarray = urlParts.slice(0, i + 1);
+            urls.push(subarray.join('/'));
+        }
+        return urls;
+    }
+
+    function getParentUrlParts(menuItem) {
+        var urlParts = menuItem.url.split('/');
+        return _.without(urlParts, urlParts[urlParts.length - 1]);
     }
 }]);
+
+storefrontApp.component('vcDocsBreadcrumbs', {
+    templateUrl: 'themes/assets/docs-page-path.tpl.html',
+    bindings: {
+        parents: '='
+    }
+});
 
 storefrontApp.component('vcDocsMenu', {
     templateUrl: 'themes/assets/docs-menu.tpl.html',
     bindings: {
         items: '=',
         url: '=',
-        parent: '=',
         isSubmenu: '='
     }
 });
