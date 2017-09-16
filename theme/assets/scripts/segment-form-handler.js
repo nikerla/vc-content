19af70c7570cc32e5843c2cbd8c99a7de4333fe6
Alexander Siniouguine <sasha@virtoway.com>
2017-09-16 00:30:08 -0700

    check for null

--- theme/assets/scripts/segment-form-handler.js
+++ theme/assets/scripts/segment-form-handler.js
@@ -1,37 +1,39 @@
 // Grab a reference to our form
 var form = $('form[name=identity-form]');
 
-// Setup a handler to run when the form is submitted
-form.on('submit', function(e) {
+if(form != null)
+{
+  // Setup a handler to run when the form is submitted
+  form.on('submit', function(e) {
 
-  // If some client-side validation kicked in and wants to prevent
-  // the form from submitting, bail out now without calling track or identify
-  if ( e.isDefaultPrevented() ) {
-    return
-  }
+    // If some client-side validation kicked in and wants to prevent
+    // the form from submitting, bail out now without calling track or identify
+    if ( e.isDefaultPrevented() ) {
+      return
+    }
 
-  // If we got here, it's okay to fire our events and submit the form      
+    // If we got here, it's okay to fire our events and submit the form      
 
-  // Stop the form from submitting...for now
-  e.preventDefault();
+    // Stop the form from submitting...for now
+    e.preventDefault();
 
-  // Identify this visitor using their email address as a distinct ID
-  // and as a new property
-  var email = form.find('[name=Contact\\[Email\\]]').val();
-  if (email) {
-    analytics.alias(email);
-    analytics.identify(email, {
-      email: email
-    });
-  }
+    // Identify this visitor using their email address as a distinct ID
+    // and as a new property
+    var email = form.find('[name=Contact\\[Email\\]]').val();
+    if (email) {
+      analytics.alias(email);
+      analytics.identify(email, {
+        email: email
+      });
+    }
 
-  // Track the event and include values from the form to our event props
-  analytics.track(eventName, formValuesToProps(form));
+    // Track the event and include values from the form to our event props
+    analytics.track(eventName, formValuesToProps(form));
 
-  // Submit the form now that all our analytics stuff is done
-  $(e.target).unbind('submit').trigger('submit');
-});
-
+    // Submit the form now that all our analytics stuff is done
+    $(e.target).unbind('submit').trigger('submit');
+  });
+}
 function addFormValuesToProps(form, props) {
     
       // When the form submit button is clicked, we want to grab all the
